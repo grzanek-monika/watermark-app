315548f47c6e192341d05952e4def10916fe53b1
Monika Grzanek <grzanekmonika@o2.pl>
2024-07-08 14:34:47 +0200

    Add addImageWatermarkToImage function to create image with watermark, add inquirer package

--- app.js
+++ app.js
@@ -13,4 +13,22 @@ const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
   await image.quality(100).writeAsync(outputFile);
 };
 
-  addTextWatermarkToImage('./programmer.jpg', './programmer-with-watermark.jpg' , 'Hello World!');
+  addTextWatermarkToImage('./programmer.jpg', './programmer-with-text-watermark.jpg' , 'Hello World!');
+
+
+  const addImageWatermarkToImage = async function(inputFile, outputFile, watermarkFile) {
+    const image = await Jimp.read(inputFile);
+    const watermark = await Jimp.read(watermarkFile);
+    const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+    const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+
+  
+    image.composite(watermark, x, y, {
+      mode: Jimp.BLEND_SOURCE_OVER,
+      opacitySource: 0.3,
+    });
+    await image.quality(100).writeAsync(outputFile);
+  };
+  
+  addImageWatermarkToImage('./programmer.jpg', './programmer-with-watermark.jpg' , './original.png');
+  
